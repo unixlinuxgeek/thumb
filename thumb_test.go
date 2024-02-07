ce0abf7f109004f11e0b69e10e4f759bff9d784a
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-02-07 16:07:07 +0500

    Added tests

--- thumb_test.go
+++ thumb_test.go
@@ -1,3 +1,6 @@
+// Work!!!
+//  ./bin/ffmpeg-amd64-static/ffmpeg -y -ss 00:01 -i /home/geek/Videos/in.mp4 -i /home/geek/Videos/in.jpg -map 1 -map 0 -c copy -disposition:0 attached_pic /home/geek/Videos/out_004.mp4
+
 package thumb
 
 import (
@@ -9,20 +12,25 @@ import (
 )
 
 func TestGen(t *testing.T) {
-	tn := time.Now()
-	u := tn.Unix()
-	tp := strconv.FormatInt(u, 10)
-	err := Gen("./test_data/in.mp4", "./test_data/out_"+tp+".png", "00:00:02")
+	var tn = time.Now()
+	var u = tn.Unix()
+	var stamp = strconv.FormatInt(u, 10)
+
+	err := Gen("./test_data/in.mp4", "./test_data/out_"+stamp+".png", "00:00:10")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if _, err := os.Stat("./test_data/out_" + tp + ".png"); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat("./test_data/out_" + stamp + ".png"); errors.Is(err, os.ErrNotExist) {
 		t.Fatal(err)
 	}
 }
 
 func TestIns(t *testing.T) {
-	err := Ins("./test_data/in.mp4", "./test_data/in.png", "./outVid.mp4", "00:01:00")
+	var tn = time.Now()
+	var u = tn.Unix()
+	var stamp = strconv.FormatInt(u, 10)
+
+	err := Ins("./test_data/in.mp4", "./test_data/out_"+stamp+".png", "./test_data/out_video_"+stamp+".mp4", "00:00:10")
 	if err != nil {
 		t.Fatal(err)
 	}
